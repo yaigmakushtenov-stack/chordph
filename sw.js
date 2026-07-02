e5f1e9265679237b4e225e8c73aa262d0d31dca8
Jun de Andres <jundeandres@gmail.com>
2026-07-02 23:28:53 +0800

    PWA: add manifest, service worker, and app icons

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = 'chordph-v1';
+var URLS_TO_CACHE = [
+    '/',
+    '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
